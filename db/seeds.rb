fdb95ff6cea86013cf9fd5e217d3bbf1f4dfa230
Ryan Searls <Ryan.searls@gmail.com>
2021-07-06 16:05:36 -0500

    added associations and post post_tag tag models

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# post = Post.create!(title: "Why Do Minnesota Sports Suck?", text: "Well it's a hard question to answer, they just suck")
+
+# post = Post.create!(title: "The Beatles are coming to Town!", text: "Coming to MPLS July 16th!")
+
+# post = Post.create!(title: "Aliigator found in the Mississippi River", text: "12 footer")
+
+post = Post.create!(title: "Snake found in Edina", text: "3 footer")
+
+
+
+# tag = Tag.create!(name: "Sports")
+# tag = Tag.create!(name: "Music")
+# tag = Tag.create!(name: "News")
+
